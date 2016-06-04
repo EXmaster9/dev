f1e2cf05e44f22e9dcd203da66bdd860a77da46a
EXmaster9 <ejlf2003@gmail.com>
2016-06-03 19:39:40 -0500

    yolomon

---
+++ Bobo/main.js
@@ -0,0 +1,16 @@
+// (           )       
+// ( )\       ( /(       
+// )((_)  (   )\())  (   
+// ((_)_   )\ ((_)\   )\  
+// | _ ) ((_)| |(_) ((_) 
+// | _ \/ _ \| '_ \/ _ \ 
+// |___/\___/|_.__/\___/ 
+// Created By: EXmaster
+// THOSE MIND BLOWING AWESOME PURE CSS GRAPHIX ALSO BY HIM!
+// EX-Games.org
+// And Finally...
+// The Code:
+var bobo = {};
+bobo.hunger = 100;
+bobo.level = 1;
+bobo.
